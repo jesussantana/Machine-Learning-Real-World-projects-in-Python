27267d68449e90a4da8c205de7b2ebe9d166f24c
jesussantana <jesus@esmarketingdigital.com>
2021-05-23 16:57:57 +0200

    Project Organization

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Build 3 Practical Real World Projects and become a master in Machine Learning',
+    author='Jesus Santana',
+    license='MIT',
+)
